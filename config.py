8e88a59a9bfda0e3412e6da5b0a4f318c6dfdb54
khadija-bit <sumeyahassan34@gmail.com>
2020-05-12 23:37:43 +0300

    Deploy to heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://khadija:newPass@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://khadija:newPass@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     QUOTE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
 
